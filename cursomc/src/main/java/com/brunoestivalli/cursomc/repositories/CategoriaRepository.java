b530d72d1d372a720a0e5ff9586b26b0f5403565
brunoestivalli <bruno.estivalli@gmail.com>
2021-04-15 09:58:43 -0300

    Curso udemy

---
+++ cursomc/src/main/java/com/brunoestivalli/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.brunoestivalli.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brunoestivalli.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+	
+	
+
+}
